b776f34129df81591ef17e1e75044d6329979d22
Ömer Okumuş <omerokumus3@gmail.com>
2025-05-11 18:07:41 +0300

    binding creation

--- app/src/main/java/com/omerokumus/androidcodesamples/presentation/MainActivity.kt
+++ app/src/main/java/com/omerokumus/androidcodesamples/presentation/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
