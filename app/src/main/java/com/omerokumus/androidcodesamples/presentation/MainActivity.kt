888b9c4a1fc3ef6613a997fa3d1431801706c35e
Ömer Okumuş <omerokumus3@gmail.com>
2025-05-11 17:50:06 +0300

    MainActivity and MainViewModel base setup

--- app/src/main/java/com/omerokumus/androidcodesamples/presentation/MainActivity.kt
+++ app/src/main/java/com/omerokumus/androidcodesamples/presentation/MainActivity.kt
@@ -1,12 +1,20 @@
-package com.omerokumus.androidcodesamples
+package com.omerokumus.androidcodesamples.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.omerokumus.androidcodesamples.R
+import com.omerokumus.androidcodesamples.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
